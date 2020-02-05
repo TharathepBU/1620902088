52d96eae9793a3eec42ade9329a868e2c42bf0b6
TharathepBU <60665913+TharathepBU@users.noreply.github.com>
2020-02-05 14:50:08 +0700

    Edit

--- hello/Quiz1.py
+++ hello/Quiz1.py
@@ -3,3 +3,10 @@ msg1 = "dfgdfgdfgfdgfdgf"
 msg2 = "dfgdfgdfgdfgdfgfdg"
 
 print(msg+msg1+msg2)
+print(msg+msg1+msg2)
+print(msg+msg1+msg2)
+print(msg+msg1+msg2)
+print(msg+msg1+msg2)
+print(msg+msg1+msg2)
+print(msg+msg1+msg2)
+print(msg+msg1+msg2)
